eb1344c85c8c42a9dec403fc3df911e290414f50
Mohasin Patel <mohasinpatel313@gmail.com>
2022-07-15 21:32:23 +0530

    Update 05_armstrong.c

--- 05_armstrong.c
+++ 05_armstrong.c
@@ -13,11 +13,62 @@
  *
  */
 
-#include<stdio.h>
+#include <stdio.h>
+
+int getNumLen(int);
+int checkArmstrong(int);
 
 int main()
 {
-    int
+    int iNum = 0, iRet = 0;
+    printf("Enter a number :-\n");
+    scanf("%d", &iNum);
+
+    iRet = checkArmstrong(iNum);
+
+    if (iRet == iNum)
+    {
+        printf("%d is a armstrong number.\n", iNum);
+    }
+    else
+    {
+        printf("%d is not an armstrong number.\n", iNum);
+    }
 
     return 0;
+}
+
+int getNumLen(int iNo)
+{
+    int iCnt = 0;
+    while (iNo != 0)
+    {
+        iNo = iNo / 10;
+        iCnt++;
+    }
+    return iCnt; // returns the number of digit enter by the user e.g. n=22 , no. of digit = 2
+}
+
+int checkArmstrong(int iNo)
+{
+    int originalNum = iNo;
+    int remainder = 0;
+    int sum = 0;
+    int mult = 1;
+    int numLen = getNumLen(iNo); // number of digits i.e. n
+    // printf("Len of digit is %d\n", numLen);
+
+    while (originalNum != 0)
+    {
+        remainder = originalNum % 10;
+        originalNum = originalNum / 10;
+
+        for (int i = 0; i < numLen; i++)
+        {
+            mult = mult * remainder; // returns multiplication of a digit n times
+        }
+        sum = sum + mult;
+        mult = 1;
+    }
+    return sum;
 }
